b2f167b710ae3e973a33ae7196edff9074c33571
Дмитрий Бороздин <borozdin1@yandex.ru>
2023-04-26 16:41:36 +0000

    Создан компонент Прелоадер На карте добавлена точка с местом проживания

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import withLayout from "../../hocs/with-layout";
-import { Map as YMap } from "@pbe/react-yandex-maps";
+import { Map as YMap, Placemark } from "@pbe/react-yandex-maps";
 import "./map.scss"
 import {ReactComponent as ChevronDownLogo} from "../../img/icons/chevron-down.svg";
 import {ReactComponent as UpdateLogo} from "../../img/icons/update.svg";
 import {ReactComponent as CloseLogo} from "../../img/icons/close.svg";
+import Preloader from "../preloader/preloader";
 
 const Map = () => {
+    const [loaded, setLoaded] = useState(true);
     return (
         <div className="map-wrap">
             <div className="map-wrap__header">
@@ -17,8 +19,27 @@ const Map = () => {
                     <CloseLogo/>
                 </div>
             </div>
-            <div className="map">
-                <YMap defaultState={{ center: [55.75, 37.57], zoom: 9 }} width={'100%'} height={'auto'}></YMap>
+            {loaded && <Preloader/>}
+            <div className="map" style={{display: loaded ? 'none':'block'}}>
+                <YMap defaultState={{ 
+                    center: [55.06, 82.9045], 
+                    zoom: 16,
+                    controls: ["zoomControl", "fullscreenControl"], 
+                    }}
+                    modules={["control.ZoomControl", "control.FullscreenControl"]}
+                    width={'100%'} 
+                    height={'auto'}
+                >
+                    <Placemark
+                        modules={["geoObject.addon.balloon"]}
+                        defaultGeometry={[55.0603, 82.9045]}
+                        properties={{
+                            balloonContentBody:
+                            "Новосибирск, ул.Дачная 21/2",
+                        }}
+                        onLoad = {() => setLoaded(false)}
+                    />
+                </YMap>
             </div>
         </div>
     )
---
+++ src/components/preloader/preloader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./preloader.scss";
+
+const Preloader = () => {
+  return (
+    <div className = "preloader" >
+      <div className = "preloader_container">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
